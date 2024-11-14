01c6addcbe78d0ae90bf134048da88dd7363a5bb
jesurythomas <jesurythomas@gmail.com>
2024-11-14 15:09:20 +0800

    Remove unused interceptor

--- src/stores/useContactStore.ts
+++ src/stores/useContactStore.ts
@@ -6,17 +6,6 @@ import { create } from 'zustand'
 import axios from 'axios'
 
 
-axios.defaults.baseURL = 'http://localhost:3000';
-
-axios.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-
 export interface ContactFormData {
     firstName: string;
     lastName: string;
